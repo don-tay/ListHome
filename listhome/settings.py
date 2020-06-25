493cfec9bf3f012980abf562c1a0035701b5af00
Don Tay Xi Cheng <dontay0209@gmail.com>
2020-06-25 19:43:37 +0800

    Add listings landlords and base templates

--- listhome/settings.py
+++ listhome/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pages.apps.PagesConfig',
+    'listings.apps.ListingsConfig',
+    'landlords.apps.LandlordsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
