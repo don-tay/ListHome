1536bdb6ae8f3efb06dcc16e4d9bb96fdf4460a3
Don Tay Xi Cheng <dontay0209@gmail.com>
2020-06-29 22:23:16 +0800

    Change to custom fields for admin dashboard landlords and listings

--- landlords/admin.py
+++ landlords/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from .models import Landlord
 
-admin.site.register(Landlord)
+class LandlordAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'hire_date')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+    list_per_page = 25
+
+admin.site.register(Landlord, LandlordAdmin)
