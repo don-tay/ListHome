297ce28b6489569a2e33292b99114e20a12d2cc5
Don Tay Xi Cheng <dontay0209@gmail.com>
2020-07-02 13:43:49 +0800

    Add about page

--- pages/views.py
+++ pages/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from listings.models import Listing
+from landlords.models import Landlord
 
 def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
@@ -13,4 +14,15 @@ def index(request):
     return render(request, 'pages/index.html', context)
 
 def about(request):
-    return render(request, 'pages/about.html')
+    # Get all landlords
+    landlords = Landlord.objects.order_by('-hire_date')
+
+    # Get MVP landlord
+    mvp_landlords = Landlord.objects.all().filter(is_mvp=True)
+
+    context = {
+        'landlords': landlords,
+        'mvp_landlords': mvp_landlords
+    }
+
+    return render(request, 'pages/about.html', context)
